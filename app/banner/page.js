513e46f12bc412cf53457c5794bc6896c26e3c23
obeespace <94024823+obeespace@users.noreply.github.com>
2023-07-31 00:32:14 +0100

    updated changes

--- app/banner/page.js
+++ app/banner/page.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 import * as htmlToImage from "html-to-image";
 import bannerpic from "../../public/mainbanner.jpeg";
 import React, { useEffect } from "react";
+import {BiArrowBack} from "react-icons/bi"
+import Link from "next/link";
 
 export default function Banner() {
   const [name, setName] = React.useState();
@@ -25,11 +27,12 @@ export default function Banner() {
   }, []);
 
   return (
-    <main className="lg:w-5/6 mx-auto lg:flex items-center justify-evenly">
-      <div id="content" className="w-[500px] relative">
+    <main className="lg:w-5/6 mx-auto items-center justify-evenly pt-20">
+      <Link href="/" className="flex items-center gap-2"><BiArrowBack />Back</Link>
+      <div id="content" className="w-[500px] mx-auto relative">
         <Image src={bannerpic} className="w-11/12 h-[460px] mx-auto" />
 
-        <div className="absolute top-[110px] left-[179px]">
+        <div className="absolute top-[110px] left-[177px]">
           <Image
             src={image}
             alt="User's Photo"
@@ -41,9 +44,9 @@ export default function Banner() {
           <p className="text-center text-white mt-4 font-semibold">{name}</p>
         </div>
       </div>
-      <div className="flex justify-center lg:w-2/6">
+      <div className="flex justify-center mt-8">
         <button
-          className="bg-violet-600 rounded-lg px-3 py-2 mt-5 text-white hover:bg-violet-900"
+          className="bg-violet-600 rounded-lg px-3 py-2 text-white hover:bg-violet-900"
           onClick={handleDownload}
         >
           Download Image
