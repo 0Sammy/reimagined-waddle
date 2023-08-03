6a948657f9316f3a32f1e93957331a034b51f242
obeespace <94024823+obeespace@users.noreply.github.com>
2023-08-03 01:02:36 +0100

    9th try

--- app/banner/page.js
+++ app/banner/page.js
@@ -15,7 +15,7 @@ export default function Banner() {
   const handleDownload = () => {
     var node = document.getElementById("content");
     htmlToImage
-      .toJpeg(node, { quality: 1 })
+      .toJpeg(node, { quality: 0.95 })
       .then(function (dataUrl) {
         var link = document.createElement("a");
         // TODO ajouter un nouveau nom de fichier
